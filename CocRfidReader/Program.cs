c1f39bebb96729b5b2161576ab8b24a98a9e6356
Radosław Radomski <48625686+RescueRangers@users.noreply.github.com>
2022-12-06 12:21:25 +0100

    Reading EPCs

--- CocRfidReader/Program.cs
+++ CocRfidReader/Program.cs
@@ -1,11 +1,85 @@
-﻿namespace CocRfidReader
+﻿using System.Collections.Concurrent;
+using CocRfidReader.Services;
+using ConcurrentObservableCollections.ConcurrentObservableDictionary;
+using Impinj.OctaneSdk;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Formatting.Compact;
+
+namespace CocRfidReader
 {
     internal class Program
     {
+        private static ConcurrentObservableDictionary<string, Tag> tags = new ConcurrentObservableDictionary<string, Tag>();
+
         static void Main(string[] args)
         {
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile(@".\Configuration\settings.json", false, true)
+                .Build();
 
+            var serviceProvider = new ServiceCollection()
+#if DEBUG
+                .AddLogging(b =>
+                {
+                    var logger = new LoggerConfiguration()
+                    .WriteTo.Console(new CompactJsonFormatter(), Serilog.Events.LogEventLevel.Verbose)
+                    .WriteTo.File(new CompactJsonFormatter(), @".\logs\log.txt", Serilog.Events.LogEventLevel.Verbose, rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+                    b.AddSerilog(logger);
+                })
+#else
+                .AddLogging(b =>
+                {
+                    var logger = new LoggerConfiguration()
+                    .WriteTo.File(new CompactJsonFormatter(), @".\logs\log.txt", Serilog.Events.LogEventLevel.Error, rollingInterval: RollingInterval.Day)
+                    .CreateLogger();
+
+                    b.AddSerilog(logger);
+                })
+#endif
+                .AddSingleton<IConfiguration>(builder)
+                .AddSingleton<ReaderService>()
+                .BuildServiceProvider();
+
+            var reader = serviceProvider.GetRequiredService<ReaderService>().Reader;
+
+            reader.TagsReported += Reader_TagsReported;
+            tags.CollectionChanged += Tags_CollectionChanged;
+
+            Console.WriteLine("Press \"R\" to start readind");
+
+            var key = Console.ReadKey();
+
+            if (key.Key == ConsoleKey.R)
+            {
+                reader.Start();
+            }
+
+            while (true)
+            {
+                Console.ReadKey();
+            }
             Console.WriteLine("Hello, World!");
+        }
+
+        private static void Tags_CollectionChanged(object? sender, DictionaryChangedEventArgs<string, Tag> e)
+        {
+            if (e.Action == System.Collections.Specialized.NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine($"Tag added with epc: {e.NewValue.Epc}");
+            }
+        }
+
+        private static void Reader_TagsReported(ImpinjReader reader, TagReport report)
+        {
+            foreach (Tag tag in report)
+            {
+                Console.WriteLine($"EPC: {tag.Epc}");
+                tags.TryAdd(tag.Epc.ToString(), tag);
+            }
         }
     }
 }
---
+++ CocRfidReader/Services/ReaderService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Impinj.OctaneSdk;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CocRfidReader.Services
+{
+    public class ReaderService
+    {
+        private string? _readerhostname;
+        private uint _readTime;
+        private int _connectTimeout;
+        private IConfiguration configuration;
+        private ILogger<ReaderService>? _logger;
+        private ImpinjReader? _reader;
+
+        public ImpinjReader Reader 
+        { 
+            get 
+            {
+                if (_reader == null || !_reader.IsConnected)
+                {
+                    ConnectToReader();
+                    ConfigureReader();
+                    return _reader;
+                }
+                
+                return _reader != null ? _reader : throw new FieldAccessException();
+            } 
+        }
+
+        public ReaderService(IConfiguration configuration, ILogger<ReaderService>? logger = null)
+        {
+            _logger = logger;
+            _readerhostname = configuration.GetValue<string>("readerIP");
+            _connectTimeout = configuration.GetValue<int>("readerTimeOut");
+            _readTime = configuration.GetValue<uint>("readerReadTime");
+            this.configuration = configuration;
+        }
+
+        private void ConnectToReader()
+        {
+            try
+            {
+                _reader ??= new ImpinjReader();
+
+                _reader.ConnectTimeout = _connectTimeout;
+                _logger?.LogDebug("Attempting connection to reader {0}", _readerhostname);
+                _reader.Connect(_readerhostname);
+                
+            }
+            catch(OctaneSdkException e)
+            {
+                _logger?.LogError(e, "Failed to connect to IP:{Address}", new { Address = _readerhostname });
+                throw;
+            }
+            
+        }
+
+        private void ConfigureReader()
+        {
+            try
+            {
+                var settings = Reader.QueryDefaultSettings();
+
+                settings.ReaderMode = ReaderMode.MaxMiller;
+                settings.Session = 2;
+                settings.SearchMode = SearchMode.SingleTarget;
+
+                settings.AutoStart.Mode = AutoStartMode.None;
+                settings.AutoStop.Mode = AutoStopMode.Duration;
+                settings.AutoStop.DurationInMs = _readTime;
+
+                settings.Keepalives.Enabled = true;
+                settings.Keepalives.PeriodInMs = 5000;
+
+                settings.Keepalives.EnableLinkMonitorMode = true;
+                settings.Keepalives.LinkDownThreshold = 5;
+
+                settings.RfMode = 0;
+
+                var antennas = configuration.GetValue("enabledAntennas", 1);
+
+                for (ushort i = 1; i <= antennas; i++)
+                {
+                    var antenna = settings.Antennas.GetAntenna(i);
+                    antenna.IsEnabled = true;
+                    antenna.MaxTxPower = true;
+                    antenna.TxPowerInDbm = configuration.GetValue($"antena{i}Power", 20.0);
+                }
+
+                var features = Reader.QueryFeatureSet();
+
+                Reader.ApplySettings(settings);
+
+                Reader.KeepaliveReceived += Reader_KeepaliveReceived;
+                Reader.ConnectionLost += Reader_ConnectionLost;
+            }
+            catch (Exception e)
+            {
+                _logger?.LogError(e, "Error while attempting to configure the reader.");
+                throw;
+            }
+        }
+
+        private void Reader_ConnectionLost(ImpinjReader reader)
+        {
+            _logger?.LogCritical("Connection lost : {0}", _readerhostname);
+            
+            reader.Disconnect();
+
+            ConnectToReader();
+        }
+
+        private void Reader_KeepaliveReceived(ImpinjReader reader)
+        {
+            _logger?.LogDebug("Keepalive received from {0}", reader.Address);
+        }
+    }
+}
