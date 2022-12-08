37064a02dd665551779fb61f55a9ba530a0aca19
Radosław Radomski <48625686+RescueRangers@users.noreply.github.com>
2022-12-08 14:41:16 +0100

    Add Packliste reader

---
+++ CocRfidReader/Services/PacklisteReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CocRfidReader.Model;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace CocRfidReader.Services
+{
+    public class PacklisteReader
+    {
+        private IConfiguration configuration;
+        private ILogger<PacklisteReader>? logger;
+
+        public PacklisteReader(IConfiguration configuration, ILogger<PacklisteReader> logger = null)
+        {
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        public async Task<IEnumerable<Coc>> GetCocs(string packingListNumber)
+        {
+            try
+            {
+                using var sql = new SqlConnection(configuration.GetValue<string>("connectionString"));
+                return await sql.QueryAsync<Coc>(@"SELECT CocNumber as PRODUKTIONSNR
+                    FROM [dbo].[uf_GetICocsOnPackingSlip] ('DAT', @PackingListNumber)", 
+                    new { PackingListNumber = packingListNumber });
+
+            }
+            catch (Exception ex)
+            {
+                logger?.LogError(ex.Message);
+                throw;
+            }
+        }
+    }
+}
