7bee485a03fa698219e6e911ffa2527cb17885a0
Radosław Radomski <48625686+RescueRangers@users.noreply.github.com>
2023-03-03 14:55:52 +0100

    Add filterable combobox to main window

---
+++ CocRfidReader.WPF/UserControls/TextBoxBaseUserChangeTracker.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls.Primitives;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CocRfidReader.WPF.UserControls
+{
+    public partial class FilterableComboBox
+    {
+        private class TextBoxBaseUserChangeTracker
+        {
+            private bool IsTextInput { get; set; }
+
+            public TextBoxBase TextBoxBase { get; set; }
+            private List<Key> PressedKeys = new List<Key>();
+            public event EventHandler UserTextChanged;
+            private string LastText;
+
+            public TextBoxBaseUserChangeTracker(TextBoxBase textBoxBase)
+            {
+                TextBoxBase = textBoxBase;
+                LastText = TextBoxBase.ToString();
+
+                textBoxBase.PreviewTextInput += (s, e) => {
+                    IsTextInput = true;
+                };
+
+                textBoxBase.TextChanged += (s, e) => {
+                    var isUserChange = PressedKeys.Count > 0 || IsTextInput || LastText == TextBoxBase.ToString();
+                    IsTextInput = false;
+                    LastText = TextBoxBase.ToString();
+                    if (isUserChange)
+                        UserTextChanged?.Invoke(this, e);
+                };
+
+                textBoxBase.PreviewKeyDown += (s, e) => {
+                    switch (e.Key)
+                    {
+                        case Key.Back:
+                        case Key.Space:
+                            if (!PressedKeys.Contains(e.Key))
+                                PressedKeys.Add(e.Key);
+                            break;
+                    }
+                    if (e.Key == Key.Back)
+                    {
+                        var textBox = textBoxBase as TextBox;
+                        if (textBox.SelectionStart > 0 && textBox.SelectionLength > 0 && (textBox.SelectionStart + textBox.SelectionLength) == textBox.Text.Length)
+                        {
+                            textBox.SelectionStart--;
+                            textBox.SelectionLength++;
+                            e.Handled = true;
+                            UserTextChanged?.Invoke(this, e);
+                        }
+                    }
+                };
+
+                textBoxBase.PreviewKeyUp += (s, e) => {
+                    if (PressedKeys.Contains(e.Key))
+                        PressedKeys.Remove(e.Key);
+                };
+
+                textBoxBase.LostFocus += (s, e) => {
+                    PressedKeys.Clear();
+                    IsTextInput = false;
+                };
+            }
+        }
+    }
+}
---
+++ CocRfidReader.WPF/UserControls/UserChange.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CocRfidReader.WPF.UserControls
+{
+    public partial class FilterableComboBox
+    {
+        private class UserChange<T>
+        {
+            private Action<T> action;
+
+            public bool IsUserChange { get; private set; } = true;
+
+            public UserChange(Action<T> action)
+            {
+                this.action = action;
+            }
+
+            public void Set(T val)
+            {
+                try
+                {
+                    IsUserChange = false;
+                    action(val);
+                }
+                finally
+                {
+                    IsUserChange = true;
+                }
+            }
+        }
+    }
+}
